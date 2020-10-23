1fd373bce681aab27a3548d63302c23271d7ecb0
ThatAnonymusGuy <37231872+ThatAnonymusGuy@users.noreply.github.com>
2020-10-23 18:39:15 +0530

    Fixed Django-related security flaw

--- Project Mistletoe/Primis/Primis/settings.py
+++ Project Mistletoe/Primis/Primis/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ikhpnp$o4tjz6(2mtozkf&tigo9r93-&6@p&4uyl3qknn!8n_1'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
